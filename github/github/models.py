79a833448bd36d9480979e118ebdc959bc52b800
popcorn <anycosmo@qq.com>
2017-12-02 20:51:17 +0800

    fix tab

--- github/github/models.py
+++ github/github/models.py
@@ -5,12 +5,13 @@ from sqlalchemy import Column, String, DateTime, Integer
 engine = create_engine('mysql+mysqldb://root:@localhost:3306/shiyanlougithub?charset=utf8')
 Base = declarative_base()
 
-class Repository(Base):
-	__tablename__ = 'repositories'
 
-	id = Column(Integer, primary_key=True)
-	name = Column(String(64))
-	update_time = Column(DateTime)
+class Repository(Base):
+    __tablename__ = 'repositories'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64))
+    update_time = Column(DateTime)
 
 if __name__ == '__main__':
-	Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
