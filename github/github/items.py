fc2fecd1d5f31a93b291c0e13eabe1437161696a
popcorn <anycosmo@qq.com>
2017-12-03 21:19:48 +0800

    scrapy challenge 3 finish

--- github/github/items.py
+++ github/github/items.py
@@ -13,3 +13,6 @@ class RepoItem(scrapy.Item):
     # name = scrapy.Field()
     name = scrapy.Field()
     update_time = scrapy.Field()
+    commits = scrapy.Field()
+    branches = scrapy.Field()
+    releases = scrapy.Field()
--- github/github/spiders/shiyanlourepos.py
+++ github/github/spiders/shiyanlourepos.py
@@ -4,8 +4,8 @@ from github.items import RepoItem
 
 class ShiyanloureposSpider(scrapy.Spider):
     name = 'shiyanlourepos'
-    allowed_domains = ['github.com']
-    start_urls = ['http://github.com/']
+    # allowed_domains = ['github.com']
+    # start_urls = ['http://github.com/']
 
     @property
     def start_urls(self):
@@ -13,7 +13,23 @@ class ShiyanloureposSpider(scrapy.Spider):
 
     def parse(self, response):
     	for repo in response.css('li.col-12'):
-	        yield RepoItem({
-	        	'name': repo.xpath('.//div[contains(@class, "mb-1")]/h3/a/text()').re_first('\s*(.+)'),
-	        	'update_time': repo.css('div.mt-2 relative-time::attr(datetime)').re('([\d-]+).([\d:]+)')
-	        	})
+            item = RepoItem()
+            item['name'] = repo.xpath('.//div[contains(@class, "mb-1")]/h3/a/text()').re_first('\s*(.+)')
+            item['update_time'] = repo.css('div.mt-2 relative-time::attr(datetime)').re('([\d-]+).([\d:]+)')
+            repo_url = response.urljoin(repo.xpath('.//div[contains(@class, "mb-1")]/h3/a/@href').extract_first())
+            request = scrapy.Request(repo_url, callback=self.parse_repo)
+            request.meta['item'] = item
+            yield request
+
+    def parse_repo(self, response):
+        item = response.meta['item']
+        data_list = response.css('li span.num::text').re('\s*(\d+)\s*')
+        item['commits'] = int(data_list[0])
+        item['branches'] = int(data_list[1])
+        item['releases'] = int(data_list[2])
+        yield item
+	        # yield RepoItem({
+	        	# 'name': repo.xpath('.//div[contains(@class, "mb-1")]/h3/a/text()').re_first('\s*(.+)'),
+	        	# 'update_time': repo.css('div.mt-2 relative-time::attr(datetime)').re('([\d-]+).([\d:]+)'),
+                # repo_url = repo.
+	        	# })
